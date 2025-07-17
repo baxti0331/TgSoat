9800080fe1dd6d28a360f615e0d4fedec3dcde88
baxti0331 <sheraliyevbaxtiyor3103@gmail.com>
2025-07-17 12:21:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,16 @@ from telethon import TelegramClient, functions, errors
 api_id = int(os.getenv('API_ID'))
 api_hash = os.getenv('API_HASH')
 phone = os.getenv('PHONE')
-code = os.getenv('TELEGRAM_CODE')  # Одноразовый код для первого входа
 
 client = TelegramClient('session_name', api_id, api_hash)
 
 async def main():
-    await client.connect()
-
-    if not await client.is_user_authorized():
-        await client.send_code_request(phone)
-        await client.sign_in(phone, code)
-        print("Вход выполнен успешно!")
-
-    prev_time = None
+    await client.start(phone)
     moscow = pytz.timezone('Europe/Moscow')
+    prev_time = None
 
     while True:
         now = datetime.datetime.now(moscow).strftime('%H:%M')
-
         if now != prev_time:
             name = f'Иван | {now}'
             try:
@@ -34,14 +26,11 @@ async def main():
                 ))
                 print(f'Имя обновлено на {name}')
                 prev_time = now
-
             except errors.FloodWaitError as e:
-                print(f'Попали в лимит! Ждем {e.seconds} секунд.')
+                print(f'Лимит! Ждём {e.seconds} секунд.')
                 await asyncio.sleep(e.seconds + 1)
-
             except Exception as e:
-                print(f'Другая ошибка: {e}')
-
+                print(f'Ошибка: {e}')
         await asyncio.sleep(30)
 
 client.loop.run_until_complete(main())
